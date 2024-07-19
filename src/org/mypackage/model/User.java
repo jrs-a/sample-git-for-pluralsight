2bc96e6709b7b7d6fc6fc8bf9c6ff19d6a92e721
jrs-a <jamaicaalberto@yahoo.com>
2024-07-19 15:37:48 +0800

    Added getter and setter for the name field.

--- src/org/mypackage/model/User.java
+++ src/org/mypackage/model/User.java
@@ -1,5 +1,15 @@
 package org.mypackage.model;
 
 public class User {
+
 	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
